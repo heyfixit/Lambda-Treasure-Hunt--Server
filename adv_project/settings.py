c5d85cbb10eaf16cc043d2f6494bc2f76a385f4c
Brady Fukumoto <bradyfukumoto@gmail.com>
2019-07-22 10:42:07 -0700

    Fixing build warnings

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -155,4 +155,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 import django_heroku
 django_heroku.settings(locals())
 
-del DATABASES['default']['OPTIONS']['sslmode'] # <-- Add this line
+# Hacky fix?
+if 'OPTIONS' in DATABASES['default']:
+    del DATABASES['default']['OPTIONS']['sslmode'] # <-- Add this line
