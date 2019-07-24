8ba0bc2d077e0a8f9727363d14f2860e45a545fb
briandoyle81 <briandoyle81@gmail.com>
2019-07-24 16:55:12 -0600

    added readme for endpoints and their use

--- adv_blockchain/urls.py
+++ adv_blockchain/urls.py
@@ -2,10 +2,9 @@ from django.conf.urls import url
 from . import api
 
 urlpatterns = [
-    # url('init', api.initialize),
     url('mine', api.mine),
-    url('new_transaction', api.new_transaction),
-    url('full_chain', api.full_chain),
+    # url('new_transaction', api.new_transaction),
+    # url('full_chain', api.full_chain),
     url('last_proof', api.last_proof),
     url('get_balance', api.get_balance)
 ]
