c4d006b54d0cb899617bc1ea7d942018f9fdfb32
Brady Fukumoto <bradyfukumoto@gmail.com>
2019-08-02 09:41:12 -0700

    Recipient

--- adv_blockchain/api.py
+++ adv_blockchain/api.py
@@ -167,11 +167,11 @@ def totals(request):
     for transaction in Transaction.objects.all():
         # Should only be one, but just in case
         recipient = transaction.recipient
-        player = User.objects.get(auth_token=recipient).player
+        # player = User.objects.get(auth_token=recipient).player
         if recipient not in total_coins:
-            total_coins[player.name] = 5
+            total_coins[recipient] = 5
         else:
-            total_coins[player.name] += 5
+            total_coins[recipient] += 5
     response = {
         'totals': total_coins
     }
